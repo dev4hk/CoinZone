b3526dbceec0dd259756827c54b99c9485b8c85b
Hoon <hoon0661@gmail.com>
2024-03-01 19:38:32 -0500

    set up state in coin page

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,64 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px 20px;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.titleFontSize};
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Subtitle = styled.h3`
+  font-size: ${(props) => props.theme.subtitleFontSize};
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const ContentsBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 20px;
+`;
 
 interface Params {
   coinId: string;
 }
 
 function Coin() {
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<Params>();
-  console.log(coinId);
-  return <h1>Coin: {coinId} </h1>;
+  useEffect(() => {
+    (async () => {
+      const info = await fetch(
+        `https://api.coinpaprika.com/v1/coins/${coinId}`
+      );
+      const price = await fetch(
+        `https://api.coinpaprika.com/v1/tickers/${coinId}`
+      );
+      const jsonInfo = await info.json();
+      const jsonPrice = await price.json();
+      setInfo(jsonInfo);
+      setPrice(jsonPrice);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Title>{loading ? "Loading..." : coinId}</Title>
+      <ContentsBox>
+        {loading ? "loading..." : <div>Coin Detail</div>}
+      </ContentsBox>
+    </Container>
+  );
 }
 
 export default Coin;
