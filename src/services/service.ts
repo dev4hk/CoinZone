1f2d14a8cb9ec91a6e4734e02dc2a22572e80a9a
Hoon <hoon0661@gmail.com>
2024-03-03 18:05:12 -0500

    added helper method to sort coin list

---
+++ src/services/service.ts
@@ -0,0 +1,8 @@
+import { ITicker } from "../interfaces/Interface";
+
+export function getSortedCoinsByPercentChanges(coins?: ITicker[]) {
+  return coins?.sort(
+    (coinA, coinB) =>
+      coinA.quotes.USD.percent_change_24h - coinB.quotes.USD.percent_change_24h
+  );
+}
